6d5a3ad93e0beffb5da278f48611f7e7bd279bad
Tirthajyoti Ghosh <itirthahere@gmail.com>
2022-01-16 22:03:00 +0530

    10 - useState Future Value

---
+++ src/components/state/User.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+type UserState = {
+    name: string
+    email: string
+}
+
+const User = () => {
+    const [user, setUser] = useState<UserState | null>(null);
+
+    const handleLogin = () => {
+        setUser({
+            name: 'John Doe',
+            email: 'john@gtmi.com'
+        });
+    };
+
+    const handleLogout = () => {
+        setUser(null);
+    };
+
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>        
+            <button onClick={handleLogout}>Logout</button>
+            
+            <p>User is {user?.email} {user?.name}</p>
+        </div>
+    );
+};
+
+export default User;
