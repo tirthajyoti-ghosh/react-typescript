9d02fa1b400b265d241c7593987f4c35a1252d89
Tirthajyoti Ghosh <itirthahere@gmail.com>
2022-01-17 01:05:26 +0530

    13 - useReducer Strict Action Types

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,10 +4,16 @@ type State = {
     count: number
 }
 
-type Action = {
-    type: string
+type UpdateAction = {
+    type: 'increment' | 'decrement'
     payload: number
 }
+
+type ResetAction = {
+    type: 'reset'
+}
+
+type Action = UpdateAction | ResetAction
 
 const initialState = {
     count: 0
@@ -19,6 +25,8 @@ function reducer(state: State, action: Action) {
             return { count: state.count + action.payload }
         case 'decrement':
             return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
         default:
             return state
     }
@@ -32,6 +40,7 @@ function Counter() {
             Counter state: {state.count}
             <button onClick={() => dispatch({ type: 'increment', payload: 10 })}>Increment by 10</button>
             <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>Decrement by 10</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
         </div>
     )
 }
